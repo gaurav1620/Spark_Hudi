2781e40fe98c4af1ec3b1aa988e17e227157feee
gaurav <gaurav>
2021-12-20 10:34:15 +0530

    exploded kafka stream

--- src/main/scala/StreamHandler.scala
+++ src/main/scala/StreamHandler.scala
@@ -1,4 +1,7 @@
 import org.apache.spark.sql._
+import scala.util.control._
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.from_json
 
 import scala.util.parsing.json._
 import org.apache.spark.sql.functions._
@@ -13,8 +16,10 @@ import org.apache.hudi.DataSourceWriteOptions
 import org.apache.hudi.DataSourceWriteOptions._
 import org.apache.hudi.config.HoodieWriteConfig
 import org.apache.hudi.hive.MultiPartKeysValueExtractor
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
 
 import java.util.logging.StreamHandler
+import scala.util.Try
 //import org.apache.hudi.config.HoodieWriteConfig._
 import org.apache.spark.sql.streaming.StreamingQueryListener.{QueryProgressEvent, QueryStartedEvent, QueryTerminatedEvent}
 
@@ -26,18 +31,17 @@ case class DeviceData(ts: String  ,board_version: String,computer_name: String,c
 object StreamHandler {
 	def main(args: Array[String]) {
 
-    def jsonToMap(js:String) : Map[String,Any] = {
-      return JSON.parseFull(js).get.asInstanceOf[Map[String,Any]]
+    def jsonToMap(js: String) : Map[String,String] = {
+      return JSON.parseFull(js).get.asInstanceOf[Map[String,String]]
     }
-//    def jsonToMap2(js:Column) : Map[String,Any] = {
-//      return js.
-////      return JSON.parseFull(js).get.asInstanceOf[Map[String,Any]]
-//    }
 
     val spark = SparkSession.builder.
       master("local")
       .appName("spark")
       .getOrCreate()
+
+      val sqlContext = new org.apache.spark.sql.SQLContext(spark.sparkContext)
+      import sqlContext.implicits._
 
     spark.sparkContext.hadoopConfiguration.setClass("mapreduce.input.pathFilter.class", classOf[org.apache.hudi.hadoop.HoodieROTablePathFilter], classOf[org.apache.hadoop.fs.PathFilter]);
 
@@ -52,7 +56,7 @@ object StreamHandler {
     val tablename = table_name
     val basepath = "file:///tmp/"+ table_name
     val basepath1 = "/tmp/"+table_name
-    val osquery_names = List("pack_system-snapshot_some_query1", "pack_system-snapshot_some_query2", "pack_system-snapshot_some_query3")
+    val osquery_names = List("pack_system-snapshot_some_query1", "pack_system-snapshot_some_query2")
     var count = 0
     val df = spark.readStream
       .format("kafka")
@@ -62,30 +66,66 @@ object StreamHandler {
       .option("startingOffsets", "earliest")
       .load()
 
-//    val dfx =  df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "CAST(topic AS STRING)", "CAST(partition AS INT)", "CAST(offset AS INT)", "CAST(timestamp AS STRING)")
-
-    //     val temp = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "CAST(topic AS STRING)", "CAST(partition AS INT)", "CAST(offset AS INT)", "CAST(timestamp AS STRING)").select("value")
-//        .foreach(x => jsonToMap(x.toString()))
-
-     val df2 =  df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "CAST(topic AS STRING)", "CAST(partition AS INT)", "CAST(offset AS INT)", "CAST(timestamp AS STRING)")
-
-//        df2.select(Value).select("snapshot").toMap
+    val df2 =  df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "CAST(topic AS STRING)", "CAST(partition AS INT)", "CAST(offset AS INT)", "CAST(timestamp AS STRING)")
       .writeStream
       .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
-        println("Writing batch : "+ batchId.toString())
+        println("Writing batch : "+ batchId.toString + "having " + batchDF.count().toString + " records.")
         batchDF.persist()
 
-        osquery_names.foreach(query_name =>{
-            batchDF
-              .filter("value like '%" + query_name+ "%'").
-                write.
+        //Filter queries
+          osquery_names.foreach(query_name =>{
+            val outLoop = new Breaks
+           outLoop.breakable{
+            val filteredDF = batchDF.filter("value like '%" + query_name+ "%'")
+            println("    " + query_name + " has " + filteredDF.count().toString + " records.")
+            if(filteredDF.count() == 0)outLoop.break()
+            val parsed = filteredDF.select("value").rdd.map(r => r(0)).collect().map(x =>x.toString)
+            val gp = parsed.map(x => x.toString.slice(x.toString.indexOf("[") + 1, x.toString.indexOf("]") ))
+            var str = "["
+            gp.foreach(x => {str = str + x.toString + ","})
+            str = str.dropRight(1)
+            str = str + "]"
+
+            if(str == "]")outLoop.break()
+
+            str = str.replaceAll("\"\"","\"temp\"")
+//            println(gp.getClass)
+//            println("Incoming " + str)
+
+            val tempStr = Seq(str)
+            val df_final = tempStr.toDF("json")
+            val schema = df_final.select(schema_of_json(df_final.select(col("json")).first.getString(0))).as[String].first
+//            println(schema.toString)
+            val expr_string = "from_json(json, '"+schema.toString + "') as parsed_json"
+//            println( "EXPR : " + expr_string)
+//            df_final.select(to_json(schema))
+//            df_final.show()
+            val parsedJson1 = df_final.selectExpr(expr_string)
+            val data = parsedJson1.selectExpr("explode(parsed_json) as json").select("json.*").withColumn("id",monotonicallyIncreasingId())
+
+            var df_1 = filteredDF.withColumn("id1",monotonically_increasing_id())
+            var df_2 =data.withColumn("id2", monotonically_increasing_id())
+
+            def hasColumn(df: DataFrame, path: String) = Try(df(path)).isSuccess
+            val hasTSasColumn = hasColumn(df_2, "timestamp")
+
+            if(hasTSasColumn == true){
+              df_2 = df_2.drop("timestamp")
+            }
+
+
+            val data_new = df_1.join(df_2,col("id1")===col("id2"),"inner")
+              .drop("id1","id2")
+
+//             data_new.show()
+            data_new.
+              write.
               format("org.apache.hudi").
               options(getQuickstartWriteConfigs).
               option("checkpointLocation", basepath).
               option(PRECOMBINE_FIELD.key(), "timestamp").
               option(RECORDKEY_FIELD.key(), "timestamp").
               option(PARTITIONPATH_FIELD.key(), "_hoodie_partition_path").
-
 //              option(OPERATION_OPT_KEY,"upsert").
               option("hoodie.datasource.write.table.type",table_type.toString()).
 //              option("hoodie.parquet.max.file.size",String.valueOf(2  * 1024 * 1024)).
@@ -96,302 +136,12 @@ object StreamHandler {
               option("hoodie.table.name", tablename.toString()).
               mode(Append).
               save(basepath+"/"+query_name)
-
-        })
-        batchDF.unpersist()
-      }
-
-      df2.start().awaitTermination()
-
-
-//      df.start().awaitTermination()
-//      .load().writeStream.format("console").start().awaitTermination()
-//      .load()
-//      .createOrReplaceTempView("data")
-
-//    osquery_names.
-//      val somequery_1 = spark.sql("select value from data where value like '%pack_system-snapshot_some_query1%' ")
-//          somequery_1.selectExpr("CAST(value AS STRING)") .writeStream.format("console").start().awaitTermination()
-
-//    df.withColumn()
-//    spark.read.format("hudi").load(basepath).show()
-
-//    val xx = spark.read.format("hudi").load(basepath).limit(1).select("value").foreach(x => println(JSON.parseFull(x(0).toString()).->("name":String)))
-//    val xx = spark.read.format("hudi").load(basepath)
-//    xx.withColumn("newCol", jsonToMap(col("value")))
-//    xx.withColumn("newCol", typedLit(jsonToMap(col("values"))))
-
-//    val xo = df.select("value").rdd.map {
-//      case Row(string_val: String) => (jsonToMap(string_val))
-//    }
-//    xo.map(xq => println(xq))
-//    xo.map(xq => println(xq.getClass))
-
-//      df.withColumn("query_name", expr("topic + value")).show()
-//    val rawDF = df.selectExpr("CAST(key AS STRING)","CAST(value AS STRING)", "CAST(topic AS STRING)",  "CAST(partition AS INT)", "CAST(offset AS INT)", "CAST(timestamp AS STRING)" );
-//          rawDF.writeStream.format("console").start().awaitTermination()
-//      val rawDF = df.selectExpr("CAST(key AS STRING)","CAST(value AS STRING)", "CAST(topic AS STRING)",  "CAST(partition AS INT)", "CAST(offset AS INT)", "CAST(timestamp AS STRING)", "CAST(query_name AS STRING)" );
-//      rawDF.withColumn("query_name", col("value"))
-//        rawDF.withColumn("query_name", expr("topic + value"))
-    //    rawDF.select("value").writeStream.format("console").start().awaitTermination()
-//
-//
-//
-//                options(getQuickstartWriteConfigs).
-//                option(PRECOMBINE_FIELD.key(), "key").
-//                option(RECORDKEY_FIELD.key(), "key").
-//                option(PARTITIONPATH_FIELD.key(), "_hoodie_partition_path").
-//                option(TBL_NAME.key(), tableName).
-//                mode(Append).
-//                save(basePath)
-//      rawDF.groupBy("value")
-
-
-
-//    rawDF.writeStream .format("hudi").
-//      option(PRECOMBINE_FIELD.key(), "timestamp").
-//      option("checkpointLocation", basepath).
-//      option(RECORDKEY_FIELD.key(), "timestamp").
-////      option(PARTITIONPATH_FIELD.key(),"timestamp").
-//      option(PARTITIONPATH_FIELD.key(), "_hoodie_partition_path").
-//      option(OPERATION_OPT_KEY,"insert").
-//      option("hoodie.insert.shuffle.parallelism","1").
-//      option("hoodie.datasource.write.table.type","COPY_ON_WRITE").
-//      option("hoodie.table.name", tablename).
-//      start(basepath)
-//      .awaitTermination()
-
-//          val query = rawDF
-//            .writeStream
-//            .foreachBatch{(batchDF: Dataset[DeviceData], batchID: Long)=>
-//      //				batchDF.createOrReplaceTempView("temp_table")
-//      //				batchDF.sparkSession.sql("select * from temp_table").show()
-//              count = count + 1
-//              println("Writing batch : " + count.toString())
-//      //				batchDF.select($"arg1").write
-//      //				batchDF.withColumn("ts", current_timestamp())
-//              batchDF.selectExpr("ts as key","board_version","computer_name","cpu_brand", "cpu_logical_cores", "cpu_microcode").write
-//                .format("hudi").
-//                options(getQuickstartWriteConfigs).
-//                option(PRECOMBINE_FIELD.key(), "key").
-//                option(RECORDKEY_FIELD.key(), "key").
-//                option(PARTITIONPATH_FIELD.key(), "_hoodie_partition_path").
-//                option(TBL_NAME.key(), tableName).
-//                mode(Append).
-//                save(basePath)
-//              println("Wrote batch " + count.toString())
-//            }
-//            .outputMode("update")
-//      //			.format("console")
-//            .start()
-//    			query.awaitTermination();
-
-//    val x = spark.read.format("hudi").load("/tmp/qa")
-//    x.show()
-//    val parsed = JSON.parseFull(x.limit(10).select($"value").head()(0).toString())
-
-
-
-
-
-//    val hudi_options = {
-//      "hoodie.table.name": "hudi_acct",
-//      "hoodie.table.type": "MERGE_ON_READ",
-//      "hoodie.datasource.write.operation": "upsert",
-//      "hoodie.datasource.write.recordkey.field": "acctid",
-//      "hoodie.datasource.write.precombine.field": "ts",
-//      "hoodie.datasource.write.partitionpath.field": "date",
-//      "hoodie.datasource.write.hive_style_partitioning": "true",
-//      "hoodie.upsert.shuffle.parallelism": 8,
-//      "hoodie.insert.shuffle.parallelism": 8,
-//    }
-//    df.writeStream
-//      .format("hudi")
-//      .outputMode("Append")
-//      .option("mergeSchema", "true")
-//      .option("checkpointLocation", basepath1)
-//      .option("hudi.table.name", tablename)
-//      .start(basepath1)
-//      .awaitTermination()
-//    df.writeStream
-//      .format("hudi")
-//      .outputMode("Append")
-//      .option("mergeSchema", "true")
-//      .option("checkpointLocation", "/tmp/osquery7")
-//      .option(TABLE_NAME, tablename)
-//      .option(RECORDKEY_FIELD_OPT_KEY, "kafka_partition_offset")
-//      .option(PARTITIONPATH_FIELD_OPT_KEY, "partition_date")
-//      .start("/tmp/osquery7")
-//      .awaitTermination()
-//    df.write.format("hudi").  options(getQuickstartWriteConfigs).  option(PRECOMBINE_FIELD_OPT_KEY, "ts").  option(RECORDKEY_FIELD_OPT_KEY, "uuid") .option(PARTITIONPATH_FIELD_OPT_KEY, "partitionpath").option("TABLE_NAME", tablename).mode(Overwrite).  save(basepath)
-
-    // read the data
-    //Currently we are reading only key and value from the table.
-//    val df1=spark.read.format("delta").option("versionAsof",1).load("/home/kushal/Desktop/tmp1/people-10m")
-//    df1.selectExpr("CAST(key AS STRING)","CAST(value AS STRING)").show(50,false)
-//    println(df1.head())
-    /*
-
-      // initialize Spark
-      val spark = SparkSession
-        .builder
-        .appName("Stream Handler")
-        .getOrCreate()
-
-
-      spark.streams.addListener(new StreamingQueryListener() {
-        override def onQueryStarted(queryStarted: QueryStartedEvent): Unit = {
-          println("Query started: " + queryStarted.id)
-        }
-        override def onQueryTerminated(queryTerminated: QueryTerminatedEvent): Unit = {
-          println("Query terminated: " + queryTerminated.id)
-        }
-        override def onQueryProgress(queryProgress: QueryProgressEvent): Unit = {
-  //				println("Query made progress: " + queryProgress.progress)
         }
       })
+        batchDF.unpersist()
+        println("Done writing the batch !")
+      }
+      df2.start().awaitTermination()
 
-
-      val tablename = "osquerydb7"
-      val basepath = "file:///tmp/osquerydb7"
-      val datagen = new datagenerator
-      val inserts = converttostringlist(datagen.generateinserts(10))
-      val df = spark.read.json(spark.sparkcontext.parallelize(inserts, 2))
-  //				df.write
-  //					 .format("hudi").
-  //				 	options(getquickstartwriteconfigs).
-  //				 	option(precombine_field.key(), "ts").
-  //				 	option(recordkey_field.key(), "uuid").
-  //				 	option(partitionpath_field.key(), "partitionpath").
-  //				 	option(tbl_name.key(), tablename).
-  //				 	mode(overwrite).
-  //				 	save(basepath)
-
-
-      import spark.implicits._
-
-      // read from Kafka
-      val inputDF = spark
-        .readStream
-        .format("kafka") // org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5
-        .option("kafka.bootstrap.servers", "localhost:9092")
-        .option("subscribe", "Orders1")
-        .option("failOnDataLoss", "false")
-        .option("startingOffsets", "earliest")
-        .load()
-
-
-//      val rawDF = inputDF.selectExpr("CAST(value AS STRING )").as[String]
-		val rawDF = inputDF.selectExpr("CAST(key AS STRING)","CAST(value AS STRING)");
-
-//		val expandedDF_Mod = rawDF.map(row => row.split(","))
-//        .map(row => row.map(x => x.split("\"")(3)))
-//      val expandedDF = rawDF.map(row => row.split(","))
-//        .map(row => DeviceData(
-//          LocalDateTime.now().toString(),
-//            row(3).split("\"")(3),
-//            row(4).split("\"")(3),
-//            row(5).split("\"")(3),
-//            row(6).split("\"")(3),
-//            row(7).split("\"")(3),
-//          "americas/brazil/sao_paul"
-//      )
-
-		rawDF.write
-			.format("hudi").
-			options(getQuickstartWriteConfigs).
-			option(PRECOMBINE_FIELD.key(), "ts").
-			option(RECORDKEY_FIELD.key(), "uuid").
-			option(PARTITIONPATH_FIELD.key(), "partitionpath").
-			option(TBL_NAME.key(), tableName)
-			.mode(Append)
-			.save(basePath)
-
-//		var count = 0
-//      val query = expandedDF
-//        .writeStream
-//  //			.trigger(Trigger.ProcessingTime("2 seconds"))
-//        .foreachBatch{(batchDF: Dataset[DeviceData], batchID: Long)=>
-//  //				batchDF.createOrReplaceTempView("temp_table")
-//  //				batchDF.sparkSession.sql("select * from temp_table").show()
-//          count+=1
-//          println("Writing batch : " + count.toString())
-//  //				batchDF.select($"arg1").write
-//  //				batchDF.withColumn("ts", current_timestamp())
-//          batchDF.selectExpr("ts as key","board_version","computer_name","cpu_brand", "cpu_logical_cores", "cpu_microcode").write
-//            .format("hudi").
-//            options(getQuickstartWriteConfigs).
-//            option(PRECOMBINE_FIELD.key(), "key").
-//            option(RECORDKEY_FIELD.key(), "key").
-//            option(PARTITIONPATH_FIELD.key(), "_hoodie_partition_path").
-//            option(TBL_NAME.key(), tableName).
-//            mode(Append).
-//            save(basePath)
-//          println("Wrote batch " + count.toString())
-//        }
-//        .outputMode("update")
-//  //			.format("console")
-//        .start()
-//			query.awaitTermination();
-  //		query.awaitTermination(10000);
-  //		spark.read
-  //			.format("org.apache.hudi")
-  //			.option("basePath", "file:///tmp/osquery1")
-  //			.load().show()
-
-  //
-  //		val dataStreamReader = spark
-  //			.readStream
-  //			.format("kafka")
-  //			.option("kafka.bootstrap.servers", "localhost:9092")
-  //			.option("subscribe", "Orders1")
-  //			.option("startingOffsets", "latest")
-  //			.option("maxOffsetsPerTrigger", 100000)
-  //			.option("failOnDataLoss", false)
-  //
-  //		val df3 = dataStreamReader.load()
-  //			.selectExpr(
-  //				"topic as kafka_topic",
-  //		"CAST(partition AS STRING) kafka_partition",
-  //		"cast(timestamp as String) kafka_timestamp",
-  //		"CAST(offset AS STRING) kafka_offset",
-  //		"CAST(key AS STRING) kafka_key",
-  //		"CAST(value AS STRING) kafka_value",
-  //		"current_timestamp() current_time"
-  //		)
-  //		.selectExpr(
-  //			"kafka_topic",
-  //		"concat(kafka_partition,'-',kafka_offset) kafka_partition_offset",
-  //		"kafka_offset",
-  //		"kafka_timestamp",
-  //		"kafka_key",
-  //		"kafka_value",
-  //		"substr(current_time,1,10) partition_date")
-  //
-  //		val query3 = df3
-  //			.writeStream
-  //			.queryName("demo")
-  //		.foreachBatch { (batchDF: DataFrame, _: Long) => {
-  //			batchDF.persist()
-  //
-  //			println(LocalDateTime.now() + "start writing cow table")
-  //			batchDF.write
-  //				.format("hudi").
-  //				options(getQuickstartWriteConfigs).
-  //				option(PRECOMBINE_FIELD.key(), "ts").
-  //				option(RECORDKEY_FIELD.key(), "uuid").
-  //				option(PARTITIONPATH_FIELD.key(), "partitionpath").
-  //				option(TBL_NAME.key(), tableName).
-  //				mode(Overwrite).
-  //				save(basePath)
-  //
-  //			println(LocalDateTime.now() + "finish")
-  //			batchDF.unpersist()
-  //		}
-  //		}
-  //			.option("checkpointLocation", "/tmp/sparkHudi/checkpoint/")
-  //			.start()
-  //
-     */
 	}
 }
