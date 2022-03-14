bf8d36df6cab5bf8bf0921fde56d46be514168a2
gaurav <gaurav>
2022-03-14 18:25:31 +0530

    new explode method, network mode set to host

--- src/main/scala/StreamHandler.scala
+++ src/main/scala/StreamHandler.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.sql._
+import org.apache.spark.sql.types.{DataType, StructType}
 import scala.util.control._
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.from_json
@@ -53,6 +54,7 @@ object StreamHandler {
       return JSON.parseFull(js).get.asInstanceOf[Map[String,String]]
     }
 
+
     val spark = SparkSession.builder.
       master("local")
       .appName("spark")
@@ -79,16 +81,18 @@ object StreamHandler {
     val basepath = "file:///tmp/"+ table_name
     val basepath1 = "/tmp/"+table_name
     val osquery_names = List("pack_system-snapshot_some_query1", "pack_system-snapshot_some_query2", "pack_system-snapshot_some_query3","pack_system-snapshot_some_query4")
-    var count = 0
-    val df = spark.readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "kafka:9092")
-      .option("subscribe", "Orders1")
-      .option("failOnDataLoss", "false")
-      .option("startingOffsets", "earliest")
-      .load()
+    //val osquery_names = List("pack_system-snapshot_some_query2")
 
+     var count = 0
+        val df = spark.readStream
+          .format("kafka")
+          .option("kafka.bootstrap.servers", "kafka:9092")
+          .option("subscribe", "Orders1")
+          .option("failOnDataLoss", "false")
+          .option("startingOffsets", "earliest")
+          .load()
     val df2 =  df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "CAST(topic AS STRING)", "CAST(partition AS INT)", "CAST(offset AS INT)", "CAST(timestamp AS STRING)")
+ 
       .writeStream
       .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
         println("Writing batch : "+ batchId.toString + "having " + batchDF.count().toString + " records.")
@@ -99,17 +103,109 @@ object StreamHandler {
             val outLoop = new Breaks
            outLoop.breakable{
             val filteredDF = batchDF.filter("value like '%" + query_name+ "%'")
+            if(filteredDF.count() == 0)outLoop.break()
+
+             val schema = new StructType()
+              .add("name",StringType)
+              .add("hostIdentifier",StringType)
+              .add("calendarTime",StringType)
+              .add("unixTime",StringType)
+              .add("epoch",StringType)
+              .add("counter",StringType)
+              .add("columns",StringType)
+              .add("action",StringType)
+
+            val explodedDF = filteredDF.
+              select(from_json(col("value"), schema).alias("value")).select("value.*")
+
+            println("*********************************************")
+            println(schema.getClass)
+            //filteredDF.show(2,false)
+            //explodedDF.select("columns").show(2,false)
+            //explodedDF.show(2,false)
+
+            //val schema2 = explodedDF.select(schema_of_json(explodedDF.select(col("columns")).first.getString(0))).as[String].first
+            val replacedString = explodedDF.select(col("columns")).first.getString(0).replaceAll("\"\"","\"temp\"")
+
+            //val json_schema = spark.read.json(explodedDF.rdd.map(lambda row: row.columns)).schema
+            //println(json_schema)
+            println("##################")
+
+
+
+            //val schema2 = DataType.fromJson(replacedString.toString)
+            val schema2 = explodedDF.select(schema_of_json(explodedDF.select(col("columns")).first.getString(0))).as[String].first
+            val expr_string = "from_json(columns, '"+schema2.toString + "') as parsed_json"
+            val finalDF = explodedDF.selectExpr(expr_string).select("parsed_json.*")
+            finalDF.
+              write.
+              format("org.apache.hudi").
+              options(getQuickstartWriteConfigs).
+              option("checkpointLocation", basepath).
+              option(PRECOMBINE_FIELD.key(), "timestamp").
+              option(RECORDKEY_FIELD.key(), "timestamp").
+              option(PARTITIONPATH_FIELD.key(), "_hoodie_partition_path").
+//              option(OPERATION_OPT_KEY,"upsert").
+              option("hoodie.datasource.write.table.type",table_type.toString()).
+//              option("hoodie.parquet.max.file.size",String.valueOf(2  * 1024 * 1024)).
+              option("hoodie.parquet.max.file.size",String.valueOf(2  * 1024 * 256)).
+//              option("hoodie.parquet.small.file.limit": "204857600").
+//              option("hoodie.parquet.max.file.size": "484402653184").
+//              option("hoodie.upsert.shuffle.parallelism","1").
+              option("hoodie.table.name", tablename.toString()).
+              mode(Append).
+              //save("http://minio:9000/"+query_name)
+              save("s3a://"+"osquery/")
+              //save("s3a://"+"osquery/" + query_name+"/")
+              //save(basepath+"/"+query_name)
+
+            //val anotherOne = explodedDF.withColumn('new_json_column', from_json(col('columns'), json_schema))
+
+           // val totallynew = explodedDF.select(from_json(col("columns"), schema2).alias("columns")).select("columns.*")
+           //val totallynew = explodedDF.select("columns.*")
+
+            //totallynew.show(2,false)
+
+            //println(explodedDF.schema)
+              //show(2,false)
+
+              /*
+              write.
+              format("org.apache.hudi").
+              options(getQuickstartWriteConfigs).
+              option("checkpointLocation", basepath).
+              option(PRECOMBINE_FIELD.key(), "timestamp").
+              option(RECORDKEY_FIELD.key(), "timestamp").
+              option(PARTITIONPATH_FIELD.key(), "_hoodie_partition_path").
+//              option(OPERATION_OPT_KEY,"upsert").
+              option("hoodie.datasource.write.table.type",table_type.toString()).
+//              option("hoodie.parquet.max.file.size",String.valueOf(2  * 1024 * 1024)).
+              option("hoodie.parquet.max.file.size",String.valueOf(2  * 1024 * 256)).
+//              option("hoodie.parquet.small.file.limit": "204857600").
+//              option("hoodie.parquet.max.file.size": "484402653184").
+//              option("hoodie.upsert.shuffle.parallelism","1").
+              option("hoodie.table.name", tablename.toString()).
+              mode(Append).
+              //save("http://minio:9000/"+query_name)
+              //save("s3://"+"osquery/")
+              save("s3a://"+"osquery/" + query_name+"/")
+
+
+
             println("    " + query_name + " has " + filteredDF.count().toString + " records.")
             if(filteredDF.count() == 0)outLoop.break()
             val parsed = filteredDF.select("value").rdd.map(r => r(0)).collect().map(x =>x.toString)
             //val gp = parsed.map(x => x.toString.slice(x.toString.indexOf("[") + 1, x.toString.indexOf("]") ))
+            print(parsed.getClass)
+
             val gp = parsed.map(x => x.toString.slice(x.toString.indexOf("{",x.toString.indexOf("{") + 1) , x.toString.indexOf("}") +1 ))
+
             var str = "["
             gp.foreach(x => {str = str + x.toString + ","})
             str = str.dropRight(1)
             str = str + "]"
 
-            if(str == "]" || (str.count(_ == 'l') >= str.length()-2))outLoop.break()
+            //if(str == "]" || (str.count(_ == 'l') >= str.length()-2))outLoop.break()
 
             str = str.replaceAll("\"\"","\"temp\"")
 //            println(gp.getClass)
@@ -117,7 +213,7 @@ object StreamHandler {
 //
             val tempStr = Seq(str)
             if(tempStr.isEmpty)outLoop.break()
-          println(str)
+            println(str)
             val df_final = tempStr.toDF("json")
             val schema = df_final.select(schema_of_json(df_final.select(col("json")).first.getString(0))).as[String].first
 //            println(schema.toString)
@@ -163,8 +259,10 @@ object StreamHandler {
               option("hoodie.table.name", tablename.toString()).
               mode(Append).
               //save("http://minio:9000/"+query_name)
+              //save("s3://"+"osquery/")
               save("s3a://"+"osquery/" + query_name+"/")
               //save(basepath+"/"+query_name)
+      */
         }
       })
         batchDF.unpersist()
